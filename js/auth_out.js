cf7b9ab58cf4bd9bb9da9e61460d6876525f0763
rahul <rahul.122293@gmail.com>
2020-01-27 01:27:07 +0530

    .

--- js/auth_out.js
+++ js/auth_out.js
@@ -1,8 +1,1 @@
 //logout users
-const logout = document.querySelector('.logout');
-logout.addEventListener('click',(e)=>{
-    e.preventDefault();
-    auth.signOut().then(()=>{
-        window.location.assign("../index.html");
-    })
-});
--- js/nav.js
+++ js/nav.js
@@ -1,40 +1,53 @@
 var nav = document.querySelector('.main-nav');
-
-firebase.auth().onAuthStateChanged(async function(user) {
+var logout;
+(async function () {
+  await firebase.auth().onAuthStateChanged(async function (user) {
     if (user) {
-        /////user is signed in
+      /////user is signed in
       var uid = user.uid;
       var isSuperAdmin;
-      await db.collection('UserProfile').where('uid','==',uid).get().then((snapshot)=>{
+      await db.collection('UserProfile').where('uid', '==', uid).get().then((snapshot) => {
         isSuperAdmin = snapshot.docs[0].data().superAdminAccess;
-    }) 
-            if(isSuperAdmin){
-          ////////////////superAdminAccess
-          nav.innerHTML = 
+      })
+      if (isSuperAdmin) {
+        ////////////////superAdminAccess
+        nav.innerHTML =
           `<ul>
-          <li><a href="../pages/superadmin.html">Home</a></li>
-          <li><a href="../pages/adminpost.html">Post A Job</a></li>
-          <li><a href="#footer">Logout</a></li>
-          <li><button class="btn btn-md btn-info mt-1 p-1" onclick="settings()"><i class="fa fa-cog"></i><b> Settings</b></button></li>
-          </ul>`;
+              <li><a href="../pages/superadmin.html">Home</a></li>
+              <li><a href="../pages/adminpost.html">Post A Job</a></li>
+              <li><a href="" class="logout">Logout</a></li>
+              <li><button class="btn btn-md btn-info mt-1 p-1" onclick="settings()"><i class="fa fa-cog"></i><b> Settings</b></button></li>
+              </ul>`;
       }
-      else{
-        nav.innerHTML = 
-        `<ul>
-        <li><a href="../pages/admin.html">Home</a></li>
-        <li><a href="../pages/adminpost.html">Post A Job</a></li>
-        <ul>`;
+      else {
+        nav.innerHTML =
+          `<ul>
+            <li><a href="../pages/admin.html">Home</a></li>
+            <li><a href="../pages/adminpost.html">Post A Job</a></li>
+            <li><a href="" class="logout">Logout</a></li>
+            <ul>`;
       }
+      //get logout
+      logout = document.querySelector('.logout');
+      logout.addEventListener('click', (e) => {
+        e.preventDefault();
+        auth.signOut().then(() => {
+          window.location.assign("../index.html");
+        })
+      });
     } else {
       // No user is signed in.
-      nav.innerHTML = 
-    `<ul>
-    <li class="active"><a href="#intro">Home</a></li>
-    <li><a href="#about">About Us</a></li>
-    <li><a href="#services">Services</a></li>
-    <li><a href="../pages/openings.html">Current Opennings</a></li>
-    <li><a href="../pages/login.html">Admin login</a></li>
-    <li><a href="#footer">Contact Us</a></li>
-    </ul>`;
+      nav.innerHTML =
+        `<ul>
+        <li class="active"><a href="#intro">Home</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="../pages/openings.html">Current Opennings</a></li>
+        <li><a href="../pages/login.html">Admin login</a></li>
+        <li><a href="#footer">Contact Us</a></li>
+        </ul>`;
     }
   });
+  //////logout users
+
+})();
