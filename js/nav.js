7cb98c9c415cacd91cd16ce9eae587e25a552676
Debraj Sengupta <theuniqueraj@gmail.com>
2020-01-27 01:37:07 +0530

    6
    6

--- js/nav.js
+++ js/nav.js
@@ -42,8 +42,8 @@ var logout;
         <li class="active"><a href="#intro">Home</a></li>
         <li><a href="#about">About Us</a></li>
         <li><a href="#services">Services</a></li>
-        <li><a href="./pages/openings.html">Current Opennings</a></li>
-        <li><a href="./pages/login.html">Admin login</a></li>
+        <li><a href="../pages/openings.html">Current Opennings</a></li>
+        <li><a href="../pages/login.html">Admin login</a></li>
         <li><a href="#footer">Contact Us</a></li>
         </ul>`;
         }
