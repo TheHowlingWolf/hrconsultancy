d4553384f008c8ed4a53209ef60af5b7426c89c3
rahul <rahul.122293@gmail.com>
2020-01-31 05:05:54 +0530

    .

--- js/normalAdmin.js
+++ js/normalAdmin.js
@@ -13,8 +13,8 @@ auth.onAuthStateChanged(user => {
                         index += 1;
 
                         await db.collection('CV').where('jid', '==', `${doc.id}`).get().then(async documents => {
-                            allJobApplicants.innerHTML += ` <div class="col-lg-4 col-md-6 col-sm-12">
-                                <div class="card text-white bg-info mb-3" style="max-width: 20rem; ">
+                            allJobApplicants.innerHTML += ` <div class="col-lg-4 col-md-6 col-sm-9 ml-auto ">
+                                <div class="card text-white bg-info mb-3 " style="max-width:30rem; ">
                                     <div class="card-header font-weight-bold text-center">${doc.data().title}</div>
                                     <div class="card-body">
                                         <p class="card-text">
